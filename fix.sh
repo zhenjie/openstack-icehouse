de59744d300d0329889b655b62bd1dee62a59f3a
Zhenjie Chen <zhenjie.chen@gmail.com>
2014-03-31 09:00:11 -0600

    Set mysql default character set to utf8

---
+++ fix.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+if [ `id -u` != 0 ]; then
+    echo "Permission denied!" 1>&2
+    exit 1
+fi
+
+# install mox, it's a bug
+# https://bugs.launchpad.net/horizon/+bug/1288245
+easy_install mox
+
+# delete old keystone db backend configuration
+sed -i 's/connection=sqlite:\/\/\/\/keystone\/openstack\/common\/db\/$sqlite_db//' /etc/keystone/keystone.conf
+sed -i 's/connection = sqlite:\/\/\/\/var\/lib\/keystone/keystone.db' /etc/keystone/keystone.conf
+
+# uninstall ubuntu-openstack-theme
+# http://docs.openstack.org/havana/install-guide/install/apt/content/install_dashboard.html
+apt-get remove --purge openstack-dashboard-ubuntu-theme
+
+# install python-prettytable, for keystone
+apt-get install python-prettytable
+
